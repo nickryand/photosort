97ab6168f3410a72cd3302f1b564b424a06b9cde
Nick Downs <nickryand@gmail.com>
2014-04-13 01:43:09 -0700

    Added directory creation with caching

--- photosort.go
+++ photosort.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"flag"
 	"os"
+	"bufio"
 	"io"
 	"hash/crc32"
+	"crypto/md5"
 	"strings"
 	"path"
+	"sort"
 )
 
 func GetTags(file *string) (*exif.Exif, error) {
@@ -26,33 +29,44 @@ func GetTags(file *string) (*exif.Exif, error) {
 	return tags, nil
 }
 
-// Return the CRC32 of a file on disk.
 func GetCrc32(file *string) (uint32, error) {
-	var checksum uint32
-	var count, total int
-
 	fp, err := os.Open(*file)
 	if err != nil {
-		return checksum, err
+		return 0, err
 	}
 	defer fp.Close()
 
-	data := make([]byte, 8192)
-	count, err = fp.Read(data)
-	for {
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return checksum, err
-		}
+	reader := bufio.NewReader(fp)
 
-		checksum = crc32.Update(checksum, crc32.IEEETable, data[:count])
+	// Calculate the CRC32 using buffered IO
+	hasher := crc32.New(crc32.IEEETable)
 
-		count, err = fp.Read(data)
-		total += count
+	_, err = io.Copy(hasher, reader)
+	if err != nil {
+		return 0, err
 	}
 
-	return checksum, nil
+	return hasher.Sum32(), nil
+}
+
+func GetMd5(file *string) (hash []byte, err error) {
+	fp, err := os.Open(*file)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer fp.Close()
+
+	reader := bufio.NewReader(fp)
+
+	// Calculate the MD5 using buffered IO
+	hasher := md5.New()
+
+	_, err = io.Copy(hasher, reader)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return hasher.Sum(nil), nil
 }
 
 // Parse the date string from an exif tag and return the year, month and day
@@ -68,10 +82,40 @@ func ParseYearMonthDay(tags *exif.Exif) (string, error) {
 	return replacer.Replace(dateSlice[0]), nil
 }
 
+// Check that a directory already exists in our cache. Using a cache to store
+// already created directories removes the need to make a system call to check
+// that the directory exists every time.
+func CheckDir(dest string, dirs []string) []string {
+	// Search for dest in our slice of strings
+	index := sort.SearchStrings(dirs, dest)
+	if index < len(dirs) && dirs[index] == dest {
+		// found it
+		return dirs
+	} else {
+		fmt.Println("Directory not in cache:", dest)
+		err := os.MkdirAll(dest, 0755)
+		if err != nil {
+			panic(err)
+		}
+				// Insert: https://code.google.com/p/go-wiki/wiki/SliceTricks
+		// This code uses the append function to grow the slice by 1 element
+		// and has the side effect of growing the underlying array if neccessary
+		dirs = append(dirs, dest)
+		copy(dirs[index+1:], dirs[index:])
+		dirs[index] = dest
+	}
+	return dirs
+}
+
 func main() {
+	// Process our command line args
 	var destpath = flag.String("destpath", ".", "Destination directory for Photos")
 	flag.Parse()
 	args := flag.Args()
+
+	// Keep a sorted list of directories we have already created
+	// so we do not need to make a system call for each picture.
+	var dirs []string
 
 	for _, filepath := range args {
 		fmt.Println("Processing file: ", filepath)
@@ -86,15 +130,18 @@ func main() {
 			panic(err)
 		}
 
+		// Check to make sure the directory exists. If not create it
 		destination := path.Join(*destpath, date)
+		dirs = CheckDir(destination, dirs)
+
 		extention := path.Ext(filepath)
 
-		crc, err := GetCrc32(&filepath)
+		hash, err := GetMd5(&filepath)
 		if err != nil {
 			panic(err)
 		}
 
-		filename := fmt.Sprintf("%X%s", crc, strings.ToLower(extention))
+		filename := fmt.Sprintf("%X%s", hash, strings.ToLower(extention))
 		fmt.Println("Copying file to: ", path.Join(destination, filename))
 	}
 }
